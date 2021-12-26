1c0dc377ab89f6680dd0e69ce5a37bc92318fa47
benhmwilfred <benhmwilfred@gmail.com>
2021-12-25 20:13:43 -0700

    Use getStaticProps to retrieve the existing channels/messages to pass as props to the main page.

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,8 @@ import React from 'react';
 import { startSignalRConnection } from '../services/signalR/signalrClient';
 import { useMessengerProvider } from '../components/Context/MessengerContext';
 import { actions } from './../reducers/message/actions';
-import axios from 'axios';
 
-const Startup = ({ channels, messages }) => {
+const Startup = () => {
   const { dispatch } = useMessengerProvider();
 
   React.useEffect(() => {
@@ -13,65 +12,12 @@ const Startup = ({ channels, messages }) => {
       type: actions.setClientConnection,
       payload: { clientConnection: startSignalRConnection() },
     });
-    dispatch({ type: actions.addChannels, payload: { channels } });
-    dispatch({
-      type: actions.updateChannelMessages,
-      payload: { messages },
-    });
   }, []);
   return (
     <div>
       <Login />
     </div>
   );
-};
-
-const getChannelMessages = async channels => {
-  return Promise.all(
-    channels.map(async c => {
-      let resp;
-      resp = await axios.get(
-        `https://localhost:5001/channels/${c.channelId}/messages`
-      );
-      return resp?.data;
-    })
-  ).then(results =>
-    results.reduce((prev, cur) => {
-      return [...prev, ...cur];
-    }, [])
-  );
-};
-
-export const getStaticProps = async () => {
-  // **** Can't use AbortController here, generates a compiler error
-  // let abortController = new AbortController()
-  //   const timeout = setTimeout(() => {
-  //       abortController.abort()
-  //       console.log("Aborted")
-  //   }, 3000)
-
-  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-  const res = await axios.get('https://localhost:5001/channels');
-  const channels = res.data;
-
-  const messages = await getChannelMessages(channels);
-
-  // let resp
-  // await axios.get(`https://localhost:5001/channels/2/messages`, {signal: abortController.signal}).then(res => {
-
-  //   clearTimeout(timeout)
-  //   resp = res}
-  //   ).catch(err => console.log(`error ${err}`))
-  // console.log(resp)
-  // console.log(`after`);
-
-  
-  return {
-    props: {
-      channels,
-      messages,
-    }, // will be passed to the page component as props
-  };
 };
 
 export default Startup;
--- pages/main.js
+++ pages/main.js
@@ -1,5 +1,4 @@
 import { Container, Row } from 'reactstrap';
-// import '../../styles.css'
 import React from 'react';
 import Footer from './../components/Footer/Footer';
 import { messageDeletedCallbackWrapper } from '../components/Messenger/messengerHelper';
@@ -8,8 +7,11 @@ import { useMessengerProvider } from '../components/Context/MessengerContext';
 import { actions } from './../reducers/message/actions';
 import ActiveChannel from './../components/ActiveChannel/ActiveChannel';
 import Layout from '../components/Layout';
+import axios from 'axios';
 
-const Main = () => {
+const Main = ({ existingChannels, existingMessages }) => {
+  console.log({existingChannels, existingMessages});
+  
   const { state, dispatch, connected } = useMessengerProvider();
 
   const { clientConnection, channels } = state;
@@ -56,13 +58,19 @@ const Main = () => {
   };
 
   React.useEffect(() => {
+    dispatch({ type: actions.addChannels, payload: { channels: existingChannels } });
+    dispatch({
+      type: actions.updateChannelMessages,
+      payload: { messages: existingMessages },
+    });
+
     if (!connected()) return;
 
     clientConnection.on('channelAdded', onChannelAddedCallback);
 
     clientConnection.on('channelDeleted', onChannelDeletedCallback);
 
-    channels.forEach(c => {
+    existingChannels.forEach(c => {
       subscribeToChannelMessageUpdates(c.channelId);
     });
 
@@ -96,6 +104,54 @@ const Main = () => {
       </Layout>
     </>
   );
+};
+
+const getChannelMessages = async channels => {
+  return Promise.all(
+    channels.map(async c => {
+      let resp;
+      resp = await axios.get(
+        `https://localhost:5001/channels/${c.channelId}/messages`
+      );
+      return resp?.data;
+    })
+  ).then(results =>
+    results.reduce((prev, cur) => {
+      return [...prev, ...cur];
+    }, [])
+  );
+};
+
+export const getStaticProps = async () => {
+  // **** Can't use AbortController here, generates a compiler error
+  // let abortController = new AbortController()
+  //   const timeout = setTimeout(() => {
+  //       abortController.abort()
+  //       console.log("Aborted")
+  //   }, 3000)
+
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+  const res = await axios.get('https://localhost:5001/channels');
+  const existingChannels = res.data;
+
+  const existingMessages = await getChannelMessages(existingChannels);
+
+  // let resp
+  // await axios.get(`https://localhost:5001/channels/2/messages`, {signal: abortController.signal}).then(res => {
+
+  //   clearTimeout(timeout)
+  //   resp = res}
+  //   ).catch(err => console.log(`error ${err}`))
+  // console.log(resp)
+  // console.log(`after`);
+
+  
+  return {
+    props: {
+      existingChannels,
+      existingMessages,
+    }, // will be passed to the page component as props
+  };
 };
 
 export default Main;
